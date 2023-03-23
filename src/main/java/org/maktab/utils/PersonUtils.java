aa1110330db629a867c85a1863c757d6fc21b819
MMahdiSheykh <momoaban@gmail.com>
2023-03-23 23:44:52 +0330

    completing PersonUtils.fillingAPersonObject

--- src/main/java/org/maktab/utils/PersonUtils.java
+++ src/main/java/org/maktab/utils/PersonUtils.java
@@ -1,4 +1,19 @@
 package org.maktab.utils;
 
+import org.maktab.entities.Person;
+import java.util.Scanner;
+
 public class PersonUtils {
+    Scanner scanner = new Scanner(System.in);
+    public Person fillingAPersonObject(){
+
+        Person person = new Person();
+
+        System.out.print("\nPlease enter the contact name : ");
+        person.setName(scanner.nextLine());
+        System.out.print("\nPlease enter the contact number : ");
+        person.setPhoneNumber(scanner.nextLine());
+
+        return person;
+    }
 }

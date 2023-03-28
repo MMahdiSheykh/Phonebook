e6b8fda88b46a58410200523dae1fe5f9e6f774e
MMahdiSheykh <momoaban@gmail.com>
2023-03-28 18:13:25 +0330

    end of the project...

--- src/main/java/org/maktab/Menu.java
+++ src/main/java/org/maktab/Menu.java
@@ -15,26 +15,46 @@ public class Menu {
                 " 2.Find a contact by name\n" +
                 " 3.Find a contact by number\n" +
                 " 4.Delete a contact by name\n" +
-                " 5.Delete a contact by number\n\n" +
+                " 5.Delete a contact by number\n" +
+                " 6.Exit\n\n" +
                 "Please enter the number of the option you want : ");
 
         int chooser = scanner.nextInt();
         switch (chooser){
             case 1:
                 personService.create();
+                waitMenu();
+                panel();
                 break;
             case 2:
                 personUtils.findPersonByName();
+                waitMenu();
+                panel();
                 break;
             case 3:
                 personUtils.findPersonByNumber();
+                waitMenu();
+                panel();
                 break;
             case 4:
                 personUtils.deletePersonByName();
+                waitMenu();
+                panel();
                 break;
             case 5:
                 personUtils.deletePersonByNumber();
+                waitMenu();
+                panel();
                 break;
+            case 6:
+                break;
+
         }
+    }
+
+    public void waitMenu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nPress enter to continue...\n");
+        scanner.nextLine();
     }
 }
